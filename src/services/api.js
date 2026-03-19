6ccb2df4f96dbbd535f92f0ead59dad73950527e
joao-ryan <joaoryanmaranhaodecastro@gmail.com>
2026-03-19 20:13:31 -0300

    atualização da API buscando repositorios e perfil

--- src/services/api.js
+++ src/services/api.js
@@ -1,11 +1,21 @@
 const BASE_URL = "https://api.github.com/users";
 
-export async function getUser(username){
-const response = await fetch(`${BASE_URL}/${username}`);
+export async function getUser(username) {
+  const response = await fetch(`${BASE_URL}/${username}`);
 
-if (!response.ok) {
+  if (!response.ok) {
     throw new Error("Usuário não encontrado");
+  }
+
+  return await response.json();
 }
 
-return await response.json();
+export async function getRepos(username) {
+  const response = await fetch(`${BASE_URL}/${username}/repos`);
+
+  if (!response.ok) {
+    throw new Error("Erro ao buscar repositórios");
+  }
+
+  return await response.json();
 }
